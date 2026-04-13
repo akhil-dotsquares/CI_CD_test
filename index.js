9082052fda958bca8b44215d2b5f94c03237da47
Akhil <akhil.priya@dotsquares.com>
2026-04-13 12:59:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3003;
+
+app.get("/", (req, res) => {
+  res.send("🚀 CI/CD Node App is running successfully!");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
